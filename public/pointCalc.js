205743fcb76ec28c90c1ffbdf9a5df50b1e8a026
Simone Cesano <simonecesano@libero.it>
2021-01-05 11:50:55 +0100

    Added straighetUp, renamed outout to parallelize

--- public/pointCalc.js
+++ public/pointCalc.js
@@ -1,8 +1,54 @@
+
 class PointCalc {
     constructor(){
 	this.input = [];
     }
-    get output(){
+
+    get frontView(){
+	var p = this.input;
+	
+	console.log(JSON.stringify(p));
+
+	var minX = Math.min(...this.input.map(p => p[0]));
+	var minY = Math.min(...this.input.map(p => p[1]));
+	var maxX = Math.max(...this.input.map(p => p[0]));
+	var maxY = Math.max(...this.input.map(p => p[1]));
+
+	var frontSideLength = Math.sqrt(Math.pow(p[0][1] - p[1][1], 2) + Math.pow(p[0][0] - p[1][0], 2));
+	var backSideLength  = Math.sqrt(Math.pow(p[2][1] - p[3][1], 2) + Math.pow(p[2][0] - p[3][0], 2));
+
+	
+	var sideDiff = frontSideLength - backSideLength;
+
+	console.log(frontSideLength, backSideLength, sideDiff)
+	
+	var p = [
+	    [minX, p[0][1]],
+	    [minX, p[1][1]],
+	    [maxX, p[1][1] + sideDiff / 4 ],
+	    [maxX, p[0][1] - sideDiff / 4 ],
+	]
+	console.log(JSON.stringify(p));
+	return p;
+    }
+
+    get straightenUp(){
+	var p = this.input;
+	
+	var minX = Math.min(...this.input.map(p => p[0]));
+	var minY = Math.min(...this.input.map(p => p[1]));
+	var maxX = Math.max(...this.input.map(p => p[0]));
+	var maxY = Math.max(...this.input.map(p => p[1]));
+
+	return [
+	    [minX, p[0][1]],
+	    [minX, p[1][1]],
+	    [maxX, p[2][1]],
+	    [maxX, p[3][1]],
+	]
+    }
+
+    get parallelize(){
 	var minX = Math.min(...this.input.map(p => p[0]));
 	var minY = Math.min(...this.input.map(p => p[1]));
 	var maxX = Math.max(...this.input.map(p => p[0]));
@@ -28,7 +74,7 @@ class PointCalc {
 	    point[2] = ang; // add the angle to the point
 	});
 
-	console.log(JSON.stringify(this.input));
+	// console.log(JSON.stringify(this.input));
 	this.input.forEach(p => { p.pop() });
 
 	return [
